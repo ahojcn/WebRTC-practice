52d5f6251b7d1672675a27d9fdb9316322530d0c
ahojcn <ahojcn@gmail.com>
2019-11-30 17:14:34 +0800

    [add] webserver/ 只采集音频数据、MediaStream获取视频约束

--- webserver/public/mediastream/js/client.js
+++ webserver/public/mediastream/js/client.js
@@ -1,5 +1,24 @@
 'use strict';
 
+let videoplay = document.querySelector('video#player');
+let audioSource = document.querySelector('select#audioSource');
+let audioOutput = document.querySelector('select#audioOutput');
+let videoSource = document.querySelector('select#videoSource');
+let filtersSelect = document.querySelector('select#filter');
+
+// picture
+let snapshot = document.querySelector('button#snapshot');
+let picture = document.querySelector('canvas#picture');
+picture.width = 320;
+picture.height = 240;
+
+// 获取音频
+// let audioplay = document.querySelector('audio#audioplayer');
+
+
+// 获取视频约束
+let divConstraints = document.querySelector('div#constraints');
+
 function start() {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     console.log('不支持');
@@ -7,12 +26,13 @@ function start() {
     let deviceId = videoSource.value;
     let constrants = {
       video: {
-        width: 320,
-        height: 240,
+        // width: 320,
+        // height: 240,
         // facingMode: 'environment',
         deviceId: deviceId ? deviceId : undefined
         // frameRate: 15
       },
+      // video: false,
       audio: {
         echoCancellation: true,
         noiseSuppression: true,
@@ -25,18 +45,18 @@ function start() {
   }
 }
 
-let videoplay = document.querySelector('video#player');
-
 function gotMediaStream(stream) {
   videoplay.srcObject = stream;
+
+  let videoTrack = stream.getVideoTracks()[0];
+  let videoConstraints = videoTrack.getSettings();
+  divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);  // 最后一个 2 表示缩进的空格
+
+  // audioplay.srcObject = stream;
 
   return navigator.mediaDevices.enumerateDevices();  // 返回一个 promise
 }
 
-let audioSource = document.querySelector('select#audioSource');
-let audioOutput = document.querySelector('select#audioOutput');
-let videoSource = document.querySelector('select#videoSource');
-let filtersSelect = document.querySelector('select#filter');
 
 function gotDevices(deviceInfos) {
   deviceInfos.forEach(function (deviceInfo) {
@@ -61,7 +81,6 @@ function handelError(err) {
 }
 
 
-
 start();
 
 // 切换 videoSource 设备触发事件
@@ -73,4 +92,15 @@ videoSource.onchange = function () {
 // filtersSelect 切换触发事件
 filtersSelect.onchange = function () {
   videoplay.className = filtersSelect.value;
+};
+
+
+// 截图点击事件
+snapshot.onclick = function () {
+  picture.className = filtersSelect.value;  // 注意只在浏览器中有滤镜效果、下载下来后不会有滤镜效果
+  picture.getContext('2d').drawImage(
+      videoplay,
+      0, 0,  // 起始点
+      picture.width, picture.height  // 宽高
+  );
 };
